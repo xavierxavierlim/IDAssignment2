d14ecb6988c550ba90cf5577dc8c72d03f36280c
Xavier Lim <s10242016@connect.np.edu.sg>
2023-02-10 00:51:21 +0800

    added comments for signup.js

--- js/signup.js
+++ js/signup.js
@@ -1,226 +1,47 @@
-
-// $(document).ready(function () {
-//     const APIKEY = "63ddca833bc6b255ed0c4632";
-//     getContacts();
-//     $("#update-contact-container").hide();
-//     // $("#add-update-msg").hide();
-//     $(".dots").hide();
-
-//     //[STEP 1]: Create our submit form listener
-//     $("#account-submit").on("click", function (e) {
-//         //prevent default action of the button 
-//         e.preventDefault();
-    
-//         //[STEP 2]: let's retrieve form data
-//         //for now we assume all information is valid
-//         //you are to do your own data validation
-//         let name = $("#name").val();
-//         let username = $("#username").val();
-//         let password = $("#password").val();
-//         let repeatPassword = $("#repeat-password").val();
-
-//         if (name == ""){
-//             alert("Name cannot be empty");
-//             return;
-//         }
-//         if (username == ""){
-//             alert("Username cannot be empty");
-//             return;z
-//         }
-//         if (password == ""){
-//             alert("Password cannot be empty");
-//             return;
-//         }
-//         if (password.length < 10){
-//             alert("Password length must be 10 characters or more");
-//             return;
-//         }
-//         if (repeatPassword == ""){
-//             alert("Repeat password cannot be empty");
-//             return;
-//         }
-//         if (password != repeatPassword){
-//             alert("Repeat password must match password");
-//             return;
-//         }
-
-//         //[STEP 3]: get form values when user clicks on send
-//         //Adapted from restdb api
-//         let jsondata = {
-//             "Name": name,
-//             "Username": username,
-//             "Password": password,
-//         };
-
-//         //[STEP 4]: Create our AJAX settings. Take note of API key
-//         let settings = {
-//             "async": true,
-//             "crossDomain": true,
-//             "url": "https://interactivedev-0fcf.restdb.io/rest/accounts",
-//             "method": "POST", //[cher] we will use post to send info
-//             "headers": {
-//             "content-type": "application/json",
-//             "x-apikey": APIKEY,
-//             "cache-control": "no-cache"
-//             },
-//             "processData": false,
-//             "data": JSON.stringify(jsondata),
-//             "beforeSend": function(){
-//             //use loading dots instead
-//             $(".dots").show();
-//             //disable our button or show loading bar
-//             // $("#account-submit").prop( "disabled", true);
-//             //clear our form using the form id and triggering it's reset feature
-//             $("#add-contact-form").trigger("reset");
-//             }
-//         }
-
-//         //[STEP 5]: Send our ajax request over to the DB and print response of the RESTDB storage to console.
-//         $.ajax(settings).done(function (response) {
-//             console.log(response);
-            
-//             $("#account-submit").prop("disabled", false);
-            
-//             //@TODO update frontend UI 
-//             // $("#add-update-msg").show()
-    
-//             //update our table 
-//             getContacts();
-//         });
-    
-//     });//end click
-
-//     //[STEP] 6
-//     //let's create a function to allow you to retrieve all the information in your contacts
-//     //by default we only retrieve 10 results
-//     function getContacts(limit = null, all = true) {
-
-//         //[STEP 7]: Create our AJAX settings
-//         let settings = {
-//             "async": true,
-//             "crossDomain": true,
-//             "url": "https://interactivedev-0fcf.restdb.io/rest/accounts",
-//             "method": "GET", //[cher] we will use GET to retrieve info
-//             "headers": {
-//               "content-type": "application/json",
-//               "x-apikey": APIKEY,
-//               "cache-control": "no-cache"
-//             },
-//         }
-
-//         //[STEP 8]: Make our AJAX calls
-//         //Once we get the response, we modify our table content by creating the content internally. We run a loop to continously add on data
-//         //RESTDb/NoSql always adds in a unique id for each data, we tap on it to have our data and place it into our links 
-
-//         $.ajax(settings).done(function (response) {
-      
-//             let content = "";
-      
-//             for (var i = 0; i < response.length && i < limit; i++) {
-
-//                 content = `${content}
-//                 <tr id='${response[i]._id}'>
-//                 <td>${response[i].name}</td>
-//                 <td>${response[i].username}</td>
-//                 <td>${response[i].password}</td>
-//                 <td><a href='#' class='delete' data-id='${response[i]._id}'>Del</a></td>
-//                 <td><a href='#update-contact-container' class='update' data-id='${response[i]._id}' data-Name='${response[i].name}' data-Username='${response[i].username}' data-Password='${response[i].password}'>Update</a></td></tr>`;
-//             }    
-        
-//             //[STEP 9]: Update our HTML content
-//             //let's dump the content into our table body
-//             $("#contact-list tbody").html(content);
-
-//             $("#total-contacts").html(response.length);
-
-//             $(".dots").hide();
-
-//             alert("You have succesfully registered for an account");
-//         });
-//     }
-
-//     //new code 
-//     var settings = {
-//         "async": true,
-//         "crossDomain": true,
-//         "url": "https://interactivedev-0fcf.restdb.io/rest/accounts",
-//         "method": "GET",
-//         "headers": {
-//           "content-type": "application/json",
-//           "x-apikey": APIKEY,
-//           "cache-control": "no-cache"
-//         }
-//     }
-    
-//     $.ajax(settings).done(function (response) {
-//         var usernameExists = response.some(function(item) {
-//             return item.Username === $('#username').val();
-//         });
-        
-//         if (usernameExists) {
-//             console.log("reached this stage");
-//             alert("This username already exists in the database.");
-//             $("#dots").hide();
-//             $("#username-warning").show();
-//         }
-//     });
-
-//     $.ajax(settings).done(function (response) {
-//         if (response.length > 0) {
-//             alert("This username is already taken");
-//             $("#dots").hide();
-//             $("#username-warning").show();
-//         }
-//     })
-
-// })
-
 $(document).ready(function () {
 
     const APIKEY = "63ddca833bc6b255ed0c4632";
-    //getContacts();
-    //$("#update-contact-container").hide();
-    // $("#add-update-msg").hide();
-    $(".dots").hide();
+    
+    $(".dots").hide(); // hide the lottie animation
 
     $("#account-submit").on("click", function (e) {
         e.preventDefault();
 
-        $(".dots").show();
-        let name = $("#name").val();
-        let username = $("#username").val();
-        let password = $("#password").val();
-        let repeatPassword = $("#repeat-password").val();
+        $(".dots").show(); // show the lottie animation
+        let name = $("#name").val(); // get the user input for name field
+        let username = $("#username").val(); // get the username input for the username field
+        let password = $("#password").val(); // get the password input for the password field
+        let repeatPassword = $("#repeat-password").val(); // get the repeat repeat input for the repeat password field
 
         // Input validation
-        if (name == "") {
+        if (name == "") { // if name empty 
             alert("Name cannot be empty");
-            $(".dots").hide();
+            $(".dots").hide(); // hide the lottie animation
             return;
         }
-        if (username == "") {
+        if (username == "") { // if username empty 
             alert("Username cannot be empty");
-            $(".dots").hide();
+            $(".dots").hide(); // hide the lottie animation
             return;
         }
-        if (password == "") {
+        if (password == "") { // if password empty
             alert("Password cannot be empty");
-            $(".dots").hide();
+            $(".dots").hide(); // hide the lottie animation
             return;
         }
-        if (password.length < 10) {
+        if (password.length < 10) { // if password length less than 10
             alert("Password length must be 10 characters or more");
-            $(".dots").hide();
+            $(".dots").hide(); // hide the lottie animation
             return;
         }
-        if (repeatPassword == "") {
+        if (repeatPassword == "") { // if repeat password length empty 
             alert("Repeat password cannot be empty");
-            $(".dots").hide();
+            $(".dots").hide(); // hide the lottie animation
             return;
         }
         if (password != repeatPassword) {
             alert("Repeat password must match password");
-            $(".dots").hide();
+            $(".dots").hide(); // hide the lottie animation
             return;
         }
 
@@ -229,7 +50,7 @@ $(document).ready(function () {
             "async": true,
             "crossDomain": true,
             "url": "https://interactivedev-0fcf.restdb.io/rest/accounts",
-            "method": "GET",
+            "method": "GET", // get the data from the database
             "headers": {
                 "content-type": "application/json",
                 "x-apikey": APIKEY,
@@ -242,29 +63,29 @@ $(document).ready(function () {
             
             let exists = false;
             for (var i = 0; i < response.length; i++) {
-                if (response[i].Username === username) {
-                    exists = true;
+                if (response[i].Username === username) { // if username input exists in the database
+                    exists = true; 
                     break;
                 }
             }
 
-            if (exists) {
+            if (exists) { // if username input exists in the database
                 alert("Username already exists. Please choose another one.");
-                $(".dots").hide();
+                $(".dots").hide(); // hide the lottie 
                 return;
-            } else {
+            } else { // if username input does not exist in the database
                 let jsondata = {
                     "Name": name,
                     "Username": username,
                     "Password": password
                 };
-                jsondata.Points = 0;
+                jsondata.Points = 0; // set the points of new accounts to 0
 
                 let settings = {
                     "async": true,
                     "crossDomain": true,
                     "url": "https://interactivedev-0fcf.restdb.io/rest/accounts",
-                    "method": "POST",
+                    "method": "POST", // post the sign up inputs to the database
                     "headers": {
                         "content-type": "application/json",
                         "x-apikey": APIKEY,
@@ -273,9 +94,8 @@ $(document).ready(function () {
                     "processData": false,
                     "data": JSON.stringify(jsondata),
                     "beforeSend": function () {
-                        // $(".dots").show();
                         alert("Sucessfully registered for an account");
-                        $(".dots").hide();
+                        $(".dots").hide(); // hide the lottie animation
                         
                         $("#add-contact-form").trigger("reset");
 
@@ -287,8 +107,8 @@ $(document).ready(function () {
                 $.ajax(settings).done(function (response) {
                     console.log(response);
                     
-                    $("#account-submit").prop("disabled", false);
-                    window.location = "../html/home.html";
+                    $("#account-submit").prop("disabled", false); 
+                    window.location = "../html/home.html"; // direct users to the home page
                     //@TODO update frontend UI 
                     // $("#add-update-msg").show()
 
