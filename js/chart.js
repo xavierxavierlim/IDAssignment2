c4ac15a34a7ca9e78220fa51ec660d5c0f0f353b
OngYiOngYi <you@example.com>
2023-02-10 17:05:14 +0800

    added comments to chart.js and map.js

--- js/chart.js
+++ js/chart.js
@@ -1,4 +1,8 @@
+
+
 var xyValues = [
+
+    // set xy values
     {x:2019, y:1000000},
     {x:2020, y:1000000000},
     {x:2021, y:15000000000},
@@ -6,10 +10,10 @@ var xyValues = [
     ];
     
     new Chart("myChart", {
-    type: "scatter",
+    type: "scatter",                    // scatter graph
     data: {
         datasets: [{
-        pointRadius: 4,
+        pointRadius: 4,                 // circle radius
         pointBackgroundColor: "rgb(0,0,255)",
         data: xyValues
         }]
@@ -17,8 +21,8 @@ var xyValues = [
     options: {
         legend: {display: false},
         scales: {
-        xAxes: [{ticks: {min: 2019, max:2023}}],
-        yAxes: [{ticks: {min: 1000000, max:20000000000}}],
+        xAxes: [{ticks: {min: 2019, max:2023}}],            // range of x axis
+        yAxes: [{ticks: {min: 1000000, max:20000000000}}],  // range of y axis
         }
     }
 });
--- js/map.js
+++ js/map.js
@@ -1,25 +1,34 @@
 
+// access key
 mapboxgl.accessToken = 'pk.eyJ1IjoiY3liZXJjYXQiLCJhIjoiY2xkbjNudWNvMGZpeTNwb3NpdXU3a3V6ciJ9.tdHX8TjZpQm9yGHvg_abBQ'
 
-navigator.geolocation.getCurrentPosition(successLocation, errorLocation, 
-    {enableHighAccuracy: true})
+// create 2 different locations
+navigator.geolocation.getCurrentPosition
+(successLocation, errorLocation, {enableHighAccuracy: true}
+)
 
+// get user's location
 function successLocation(position){
     setupMap([position.coords.longitude, position.coords.latitude])
 }
+
+// if error, use this location
 function errorLocation(){
     setupMap([1.3521, 103.8198])
 }
 
+
 function setupMap(center)
 {
+    // create the map
     const map = new mapboxgl.Map({
         container: 'map',
         style: 'mapbox://styles/mapbox/streets-v11',
         center: center,
-        zoom: 9,
+        zoom: 9,        // show whole singapore
     });
 
+    // add navigation to map
     const nav = new mapboxgl.NavigationControl()
     map.addControl(nav)
 
@@ -27,46 +36,4 @@ function setupMap(center)
         accessToken: mapboxgl.accessToken
     })
     map.addControl(directions,"top-left")
-
-    map.on('load', () => {
-        // Add the vector tileset as a source.
-        map.addSource('ethnicity', {
-        type: 'vector',
-        url: 'mapbox://examples.8fgz4egr'
-        });
-        map.addLayer(
-            {
-            'id': 'population',
-            'type': 'circle',
-            'source': 'ethnicity',
-            'source-layer': 'sf2010',
-            'paint': {
-            // Make circles larger as the user zooms from z12 to z22.
-            'circle-radius': {
-            'base': 1.75,
-            'stops': [
-            [12, 2],
-            [22, 180]
-            ]
-            },
-            // Color circles by ethnicity, using a `match` expression.
-            'circle-color': [
-            'match',
-            ['get', 'ethnicity'],
-            'White',
-            '#fbb03b',
-            'Black',
-            '#223b53',
-            'Hispanic',
-            '#e55e5e',
-            'Asian',
-            '#3bb2d0',
-            /* other */ '#ccc'
-            ]
-            }
-            },
-            // Place polygons under labels, roads and buildings.
-            'aeroway-polygon'
-            )
-        })
 }
